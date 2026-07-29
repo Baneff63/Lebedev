a1d528008a6e7d9ef596695c9d1abc9cb5df13d0
Lebedev Daniil <126359268+Baneff63@users.noreply.github.com>
2026-07-29 11:40:44 +0400

    Delete site/src directory

--- site/src/app/api/content/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { readSiteData } from "@/lib/site-data";
-
-export async function GET() {
-  const data = await readSiteData();
-  return NextResponse.json(data);
-}
